fff0053ee574db15c1fa4e7fff9cd4905f9e2fba
ASTedgar <cesk002@gmail.com>
2013-11-08 14:31:21 +0100

    modified:   pyactive_project/examples/test_multi/test_multi.py 	modified:   pyactive_project/pyactive/Multi/__init__.py 	pyactive_project/examples/my_paxos/

--- pyactive_project/examples/test_multi/test_multi.py
+++ pyactive_project/examples/test_multi/test_multi.py
@@ -1,33 +1,55 @@
 '''
 @author: Edgar Zamora Gomez <edgar.zamora@urv.cat>
 '''
-from pyactive.controller import init_host, serve_forever, start_controller
-
-from pyactive.Multi import AMulti
+from pyactive.controller import init_host, serve_forever, start_controller, sleep
+import time
+from pyactive.Multi import AMulti, SMulti
 
 class Server(object):
     _sync = {}
-    _async = ['add_actors', 'add']
+    _async = ['add_actors', 'add', 'sync_add','print_references']
     _ref = ['add_actors']
     _parallel = []
     
     def add_actors(self, list_actors):
-        self.amulti = AMulti(list_actors, self._atom)
+        self.amulti = AMulti(list_actors)
+        self.smulti = SMulti(list_actors, self._atom)
     
     def add(self, x, y):
         self.amulti.add(x, y)
-        
-
+    
+    def sync_add(self, x, y):
+        result =  self.smulti.sync_add(x, y)
+        print 'siiiiiiiiiiiiiiiiiiiiii', result
+    def print_references(self):
+        print self.smulti.get_reference()
 class Calc():
-    _sync = {}
+    _sync = {'sync_add':'1', 'get_reference':'1'}
     _async = ['add']
-    _ref = []
+    _ref = ['get_reference']
     _parallel = []
         
     def add(self, x, y):
         print x+y
         
+    def sync_add(self, x, y):
+        sleep(2)
+        return x+y
+    def get_reference(self):
+        return self.proxy
+class Calc1():
+    _sync = {'sync_add':'1', 'get_reference':'1'}
+    _async = ['add']
+    _ref = ['get_reference']
+    _parallel = []
         
+    def add(self, x, y):
+        print x+y
+        
+    def sync_add(self, x, y):
+        return x+y 
+    def get_reference(self):
+        return self.proxy       
 def test():
     
     calcs = []
@@ -35,15 +57,18 @@ def test():
     tcpconf = ('tcp',('127.0.0.1',6664))
     host = init_host(tcpconf)
     
-    for i in range(4):
+    for i in range(1):
         calcs.append(host.spawn_id(str(i),'test_multi','Calc',[]))
+    
+    for i in range(2):
+        calcs.append(host.spawn_id(str(i),'test_multi','Calc1',[]))
     
     server = host.spawn_id('serv', 'test_multi', 'Server', [])
     server.add_actors(calcs)
     
     server.add(4,4)
-    
-  
+    server.sync_add(3,3)
+    server.print_references()
    
 def main():
     start_controller('pyactive_thread')
--- pyactive_project/pyactive/Multi/__init__.py
+++ pyactive_project/pyactive/Multi/__init__.py
@@ -2,53 +2,96 @@
 Author: Edgar Zamora Gomez  <edgar.zamora@urv.cat>
 """
 from pyactive.constants import *
-from pyactive.controller import later, send_timeout, new_group
+from pyactive.controller import later, send_timeout, new_group, send_timeout_multi, get_host
+from pyactive.exception import PyactiveError, TimeoutError
 from time import sleep
 import uuid
+import copy
+import exceptions
 
-class AMulti(object):
-    def __init__(self, list_actors, atom):
-        self.actors = list_actors
-        self.own_actor = atom
+class AbstractMulti(object):
+    def __init__(self, list_actors):
+        self.list_actors = []
+        self.list_actors = list_actors
         
+    def attach(self, actor):
+        self.list_actors.extend(actor)
+    
+    def detach(self, actor):
+        self.list_actors.remove(actor)
         
+class AMulti(AbstractMulti):
+    def __init__(self, list_actors):
+        AbstractMulti.__init__(self, list_actors)
     def __getattr__(self, name):
         return _RemoteMethod2(self.dispatch, name)
     
         
     def dispatch(self, methodname, vargs, kwargs):
-        for a in self.actors:
-            a.async_remote_call(methodname, vargs, kwargs)
+        for a in self.list_actors:
+            getattr(a, methodname)(*vargs)
+#             a.async_remote_call(methodname, vargs, kwargs)
+    
+class SMulti(AbstractMulti):
+    
+    def __init__(self, list_actors, atom):
+        AbstractMulti.__init__(self, list_actors)
+        self.own_actor = atom
+        self.actor = new_group(self.own_actor.aref)
+        self.ref_list = self.list_actors[0].refSync
+        for name in self.ref_list:
+            setattr(self, name, _RefWraper(self.dispatch, name))
+                        
+    def __getattr__(self, name):
+        return _RemoteMethod2(self.dispatch, name)
+    
+    def dispatch(self, methodname, vargs, kwargs):
+        
+        result = []
+        rpc_id_list = []
+        msg = {}
+        msg[METHOD] = methodname
+        msg[PARAMS] = vargs
+        msg[MODE] = SYNC
+        msg[FROM] = self.actor.aref
+        msg[TYPE] = CALL
+        rpc_id = str(uuid.uuid1())
+        msg[SRC] = self.actor.channel
+        later(int(self.list_actors[0].syncList.get(methodname)), send_timeout_multi, self.actor.channel, rpc_id_list)
+        for a in self.list_actors:
+            msg[TO] = a.client.aref
+            rpc_id = str(uuid.uuid1())
+            msg[RPC_ID] = rpc_id
+            a.client.set_pending(msg[RPC_ID])
+            rpc_id_list.append(rpc_id) 
+            msg2 = copy.copy(msg)
+            a.client.out.send(msg2)
+        for a in self.list_actors:
+            parcial_result =  self.actor.receive_result()
+            if isinstance(parcial_result, PyactiveError):
+                if not result:
+                    raise Exception, 'The timeout has expired'
+                else:
+                    return result
+            
+            result.append(parcial_result)
+        return result
 
+class _RefWraper():
+    def __init__(self, send, name):
+        self.__send = send
+        self.__name = name
+    def __call__(self, *args, **kwargs):
+        new_args = get_host()._dumps(list(args))
+        result = self.__send(self.__name, new_args, kwargs)
+        if result != None:
+            return get_host()._loads(result)
+        else:
+            return result
 class _RemoteMethod2():
     def __init__(self, send, name):
         self.__send = send
         self.__name = name
         
     def __call__(self, *args, **kwargs):
-        return self.__send(self.__name, args, kwargs)
-    
-class SMulti(object):
-    
-    def __init__(self, list_actors, atom):
-        self.actors = list_actors
-        self.own_actor = atom
-        self.actor = new_group(self.own_actor.aref)
-        
-    def __getattr__(self, name):
-        return _RemoteMethod2(self.dispatch, name)
-    
-    def dispatch(self, methodname, vargs, kwargs):
-        msg = {}
-        msg[METHOD] = methodname
-        msg[PARAMS] = vargs
-        msg[MODE] = SYNC
-        msg[FROM] = self.actor.aref
-        rpc_id = str(uuid.uuid1())
-        msg[RPC_ID] = rpc_id
-        msg[SRC] = self.actor.channel
-        later(self.actors[0].syncList.get(methodname), send_timeout(), self.own_actor.channel, rpc_id)
-        for a in self.actors:
-            a.sync_remote_call(methodname, vargs, kwargs)
-        
-        
+        return self.__send(self.__name, args, kwargs)        
