1c48ef804856cb663953d98d13c2a4ef491ddc9b
pedrotgn <astlab144>
2016-02-05 12:35:55 +0100

    seld.id example added

---
+++ pyactive_project/examples/tutorial/actor1b.py
@@ -0,0 +1,28 @@
+"""
+Author: Pedro Garcia Lopez <pedro.garcia@urv.cat>
+"""
+from pyactive.controller import init_host, launch,start_controller, sleep
+
+
+class Echo:
+    _sync = {}
+    _async = ['echo']
+    _parallel = []
+    _ref = []
+
+    def echo(self,msg):
+        print "actor",self.id, msg
+
+def test():
+    host = init_host()
+    e1 = host.spawn_id('1', 'actor1b', 'Echo', [])
+
+    e1.echo('hola')
+    e1.echo('adios')
+
+
+
+
+if __name__ == '__main__':
+    start_controller('pyactive_thread')
+    launch(test)
